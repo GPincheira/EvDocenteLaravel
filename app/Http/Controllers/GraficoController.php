809fbe2d2980b4a6037a7cfdfd7e8fa169416865
GPincheira <german1234esteban56@gmail.com>
2020-08-11 03:44:12 -0400

    Selectores para graficos

--- app/Http/Controllers/GraficoController.php
+++ app/Http/Controllers/GraficoController.php
@@ -6,6 +6,7 @@ use App\evaluacion;
 use App\departamento;
 use App\facultad;
 use App\academico;
+use App\Proceso;
 use DB;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -14,56 +15,11 @@ class GraficoController extends Controller
 {
   public function principal(Request $request)
   {
-/*    $CodigoFacultad = @Auth::user()->secFacultad->CodigoFacultad;
-//    $departamentos = Departamento::all()->where('CodigoFacultad',$CodigoFacultad);
-
-    $totales = DB::table('evaluaciones')->select(DB::raw('sum(p1)'));
-
-
-    $lala = Evaluacion::join('')->sum('p1')->where('CodigoFacultad',$CodigoFacultad);
-
-    dd($totales);
-//    $balance = DB::table('data')->sum('balance')->where('user_id' '=' $id);
-//    SELECT Sum(PrecioUnidad * Cantidad) AS Total FROM DetallePedido;
-
-    return view('graficos.principal',compact('departamentos'));*/
-
-  $evaluaciones=Evaluacion::where('CodigoFacultad','=',@Auth::user()->secFacultad->CodigoFacultad)->get();
-  $datosGrafico = new \stdClass;
-  $datosGrafico->totalActividad1=0;
-  $datosGrafico->totalActividad2=0;
-  $datosGrafico->totalActividad3=0;
-  $datosGrafico->totalActividad4=0;
-  $datosGrafico->totalActividad5=0;
-
-  foreach($evaluaciones as $dato){
-    if($dato->tiempoActividad1!=""){
-      $datosGrafico->totalActividad1=$datosGrafico->totalActividad1+$dato->tiempoActividad1;
-    }
-    if($dato->tiempoActividad2!=""){
-      $datosGrafico->totalActividad2=$datosGrafico->totalActividad2+$dato->tiempoActividad2;
-    }
-    if($dato->tiempoActividad3!=""){
-      $datosGrafico->totalActividad3=$datosGrafico->totalActividad3+$dato->tiempoActividad3;
-    }
-    if($dato->tiempoActividad4!=""){
-      $datosGrafico->totalActividad4=$datosGrafico->totalActividad4+$dato->tiempoActividad4;
-    }
-    if($dato->tiempoActividad5!=""){
-      $datosGrafico->totalActividad5=$datosGrafico->totalActividad5+$dato->tiempoActividad5;
-    }
+    $CodigoFacultad = @Auth::user()->secFacultad->CodigoFacultad;
+    $departamentos = Departamento::all()->where('CodigoFacultad',$CodigoFacultad);
+    $procesos = proceso::all();
+    $año = $request['año'];
+    $dpto = $request['departamento'];
+    return view('graficos.principal',compact('departamentos', 'procesos', 'año', 'dpto'));
   }
-  $departamentos=Departamento::where('CodigoFacultad','=',@Auth::user()->secFacultad->CodigoFacultad)->get();
-  $seleccionado = new \stdClass;
-  $seleccionado->periodo="";
-  $seleccionado->departamento="";
-
-  $academicos=Academico::join('departamentos as d1', 'academicos.CodigoDpto','=','d1.id')->where('d1.CodigoFacultad','=',@Auth::user()->secFacultad->CodigoFacultad)->get();
-  if($evaluaciones=="[]"){
-    return view('graficos.principal',compact('datosGrafico','departamentos','seleccionado','academicos'))->with('Mensaje','No hay datos para mostrar');
-  }else{
-    dd($academicos);
-    return view('graficos.principal',compact('datosGrafico','departamentos','seleccionado','academicos'));
-  }
-}
 }
--- resources/views/graficos/principal.blade.php
+++ resources/views/graficos/principal.blade.php
@@ -12,7 +12,7 @@
 </nav>
 
 <div class="row">
-  <div class="col-lg-12 margin-tb">
+  <div class="col-xs-12 col-sm-12 col-md-12">
     <div class="pull-left">
       <h2>Gráficos</h2>
     </div>
@@ -25,5 +25,32 @@
   </div>
 @endif
 
+<div class="form-row">
+  <div class="col-xs-12 col-sm-12 col-md-12">
+      <form action="{{ route('graficos.principal') }}" method="GET">
+          <div class="col-xs-4 col-sm-4 col-md-4">
+            <h5>Año</h5>
+            <select name="año" class="form-control">
+              @foreach($procesos as $proceso)
+                 <option value='{{$proceso->año}}' @if($proceso->año==$año) selected @endif>{{$proceso->año}}</option>
+              @endforeach
+            </select>
+          </div>
+          <div class="col-xs-7 col-sm-7 col-md-7">
+            <h5>Departamento</h5>
+            <select name="departamento" class="form-control">
+              <option value="">TODOS LOS DEPARTAMENTOS</option>
+              @foreach($departamentos as $departamento)
+                 <option value='{{$departamento->id}}' @if($departamento->id==$dpto) selected @endif>{{$departamento->Nombre}}</option>
+              @endforeach
+            </select>
+          </div>
+          <div class="col-xs-1 col-sm-1 col-md-1">
+              <br><button type="submit" class="btn btn-primary">Graficar</button>
+          </div>
+        </form>
+      </div>
+  </div>
+</div>
 
 @endsection
